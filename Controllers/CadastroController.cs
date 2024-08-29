a18337c579a6dbf2d7276ea606467bbd3efddf96
Rodrigo Mota <rodrigo.mota@dbccompany.com.br>
2024-08-28 21:35:53 -0300

    primeiro commit

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace encopav_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CadastroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Teste()
+        {
+            return Ok("Rodando...");
+        }
+    }
+}
